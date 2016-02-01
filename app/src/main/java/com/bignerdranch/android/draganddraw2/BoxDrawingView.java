ee9852fba2a88018efd022ca362d71e6de7356c0
iceguyvn <nguyen_my@yahoo.com>
2016-02-01 14:04:30 -0800

    logged x-y coordinates of ACTION_DOWN, ACTION_MOVE, ACTION_UP, ACTION_CANCEL

--- app/src/main/java/com/bignerdranch/android/draganddraw2/BoxDrawingView.java
+++ app/src/main/java/com/bignerdranch/android/draganddraw2/BoxDrawingView.java
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.draganddraw2;
 
 import android.content.Context;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -9,6 +12,8 @@ import android.view.View;
  */
 // this class is a custom view that derives from View which is a blank canvas
 public class BoxDrawingView extends View {
+   public static final String    TAG = "BoxDrawingView";
+
    // this method is used when creating the view in code
    public BoxDrawingView(Context context) {
       this(context, null);
@@ -17,5 +22,27 @@ public class BoxDrawingView extends View {
    // this method is used when inflating the view from XML
    public BoxDrawingView(Context context, AttributeSet attributeSet) {
       super(context, attributeSet);
+   }
+
+   @Override
+   public boolean onTouchEvent(MotionEvent event) {
+      PointF current = new PointF(event.getX(), event.getY());
+      String action = "";
+      switch (event.getAction()) {
+         case MotionEvent.ACTION_DOWN:
+            action = "ACTION_DOWN";
+            break;
+         case MotionEvent.ACTION_MOVE:
+            action = "ACTION_MOVE";
+            break;
+         case MotionEvent.ACTION_UP:
+            action = "ACTION_UP";
+            break;
+         case MotionEvent.ACTION_CANCEL:
+            action = "ACTION_CANCEL";
+            break;
+      }
+      Log.i(TAG, action + " at x=" + current.x + ", y=" + current.y);
+      return true;
    }
 }
